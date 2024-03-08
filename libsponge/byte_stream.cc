649fb4c6d04cdc61801f2b905c2ba670de120c1b
Aurora <3518794353@qq.com>
2024-03-08 13:12:58 +0800

    modified:   apps/webget.cc 	modified:   libsponge/byte_stream.cc 	modified:   libsponge/byte_stream.hh

--- libsponge/byte_stream.cc
+++ libsponge/byte_stream.cc
@@ -39,7 +39,7 @@ size_t ByteStream::write(const string &data) {
 }
 
 //! \param[in] len bytes will be copied from the output side of the buffer
-string ByteStream::peek_output(const size_t len) const {
+std::string ByteStream::peek_output(const size_t len) const {
     string output = "";
     auto numReaded = buffer_size();
     unsigned int n = min(len, numReaded);
