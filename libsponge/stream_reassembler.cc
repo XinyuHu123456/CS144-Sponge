8da836cb46363710449142fe4143d9a75fea5c74
Aurora <3518794353@qq.com>
2024-03-15 00:59:48 +0800

    new file:   libsponge/a 	modified:   libsponge/stream_reassembler.cc

--- libsponge/stream_reassembler.cc
+++ libsponge/stream_reassembler.cc
@@ -1,5 +1,6 @@
 #include "stream_reassembler.hh"
 
+#include <iostream>
 // Dummy implementation of a stream reassembler.
 
 // For Lab 1, please replace with a real implementation that passes the
@@ -28,8 +29,19 @@ void StreamReassembler::push_substring(const string &data, const size_t index, c
         _eof = true;
         _eof_index = data.length() + index;
     };
+    // 定义一个减少的长度
+    int sub_length = 0;
+    /*
+    这里出现一个问题，就是有一种可能情况:
+    当first_unassembled <= index < first_unassembled() && index + data.length() >= first_unassembled()
+    如果这个数据segment中的eof == true会导致递归调用push_substring()，并且此时eof == true
+    再次调用这个函数的时候，修改_eof_index的值，导致结果出错
 
-    //分析输入substring与窗口的可能边界条件
+    怎么去解决这个问题？
+    如果eof == true就不进行递归调用，只是直接把数据切割————无法解决
+    */
+
+    // 分析输入substring与窗口的可能边界条件
     if (index >= first_unacceptable() || index < first_unassembled() ||
         (index < first_unacceptable() && index + data.length() > first_unacceptable())) {
         if (index < first_unassembled() && index + data.length() >= first_unassembled())
@@ -37,7 +49,10 @@ void StreamReassembler::push_substring(const string &data, const size_t index, c
             // 的地方开始并不意味着，在该字符串中的索引为first_unassembled()的子字符串开始
             return push_substring(data.substr(first_unassembled() - index, data.length()), first_unassembled(), eof);
         else if (index < first_unacceptable() && index + data.length() > first_unacceptable()) {
-            return push_substring(data.substr(0, first_unacceptable()-index), index, eof);
+            if (eof == true) {
+                sub_length = data.length() - (first_unacceptable() - index);
+            } else
+                return push_substring(data.substr(0, first_unacceptable() - index), index, eof);
         } else {
             return;
         }
@@ -45,7 +60,7 @@ void StreamReassembler::push_substring(const string &data, const size_t index, c
     // 如果第一个数据包已经到达
     if (index == first_unassembled())
         first_in = true;
-    for (size_t i = 0; i < data.length(); i++) {
+    for (size_t i = 0; i < data.length() - sub_length; i++) {
         // 如果出现数据不一致的情况，panic
         if (buffer[(index + i) % _capacity].second == true) {
             if (buffer[(index + i) % _capacity].first == data[i])
@@ -76,7 +91,7 @@ void StreamReassembler::push_substring(const string &data, const size_t index, c
         _unassembled_bytes -= i;
         _output.write(output_string);
     };
-    if (first_unassembled() == _eof_index && _eof) {
+    if ((first_unassembled() == _eof_index) && _eof) {
         _output.end_input();
     };
 }
@@ -90,3 +105,37 @@ size_t StreamReassembler::first_unacceptable() { return _output.bytes_read() + _
 size_t StreamReassembler::unassembled_bytes() const { return {_unassembled_bytes}; }
 
 bool StreamReassembler::empty() const { return {_unassembled_bytes == 0}; }
+
+// 将第一个测试用例写入代码,通过GDB进行调试
+/*
+int main() {
+    // 先初始化一下 StreamReassembler
+    StreamReassembler streamReassembler = StreamReassembler(8);
+    // 写入测试用例的相关操作
+    streamReassembler.push_substring("abc", 0, false);
+    cout << streamReassembler.stream_out().eof() << endl;
+    streamReassembler.push_substring("ghX", 6, true);
+    cout << streamReassembler.stream_out().eof() << endl;
+    streamReassembler.push_substring("cdefg", 2, false);
+    streamReassembler.stream_out().read(8);
+    cout << streamReassembler.stream_out().eof() << endl;
+}
+*/
+
+/*
+      {
+            ReassemblerTestHarness test{8};
+
+            test.execute(SubmitSegment{"abc", 0});
+            test.execute(BytesAssembled(3));
+            test.execute(NotAtEof{});
+
+            test.execute(SubmitSegment{"ghX", 6}.with_eof(true));
+            test.execute(BytesAssembled(3));
+            test.execute(NotAtEof{});
+
+            test.execute(SubmitSegment{"cdefg", 2});
+            test.execute(BytesAssembled(8));
+            test.execute(BytesAvailable{"abcdefgh"});
+            test.execute(NotAtEof{});
+        }*/
